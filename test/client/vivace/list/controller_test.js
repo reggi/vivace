63b10a522b8f1332853dbf4d72cd896ab3049036
Josh Shor <jpshor@gmail.com>
2015-12-14 15:32:28 -0500

    updates tests to reflect file system

--- test/client/vivace/list/controller_test.js
+++ test/client/vivace/list/controller_test.js
@@ -1,11 +1,11 @@
 const chai =  require('chai');
 
 
-describe('the candidateList controller', () => {
+describe('the vivace list controller', () => {
   let ctrl;
   beforeEach(angular.mock.module('irCandidateList'));
   beforeEach(angular.mock.inject($controller => {
-    ctrl = $controller(require('../../../client/candidateList/controller'));
+    ctrl = $controller(require('../../../../client/vivace/list/controller'));
   }));
 
   it('should not be null', () => {
