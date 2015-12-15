3aa7ed8bb9ba64d8044bd0c7d4740b1d9865916a
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-12-15 16:26:10 -0500

    -Adding a candidate works (uses populate method)

--- server/helper/database.js
+++ server/helper/database.js
@@ -1,9 +1,39 @@
 import redis from 'redis';
+import flatten from 'flat';
+import bluebird from 'bluebird';
 
-var client = redis.createClient();
+class DbHelper {
 
-client.on('error', function (err) {
-  console.log('Error ' + err);
-});
+  constructor() {
+    //Promisifying the redis library
+    bluebird.promisifyAll(redis.RedisClient.prototype);
+    bluebird.promisifyAll(redis.Multi.prototype);
 
-module.exports = client;
+    this.client = redis.createClient("32768", "192.168.99.100");
+
+    this.client.on('error', (err) => {
+      console.log('Error ' + err);
+    });
+  }
+
+  add(model, obj) {
+    let flatObj = flatten(obj);
+
+    return this.client.incrAsync(model.getKey() + "_counter").then((counter)=> {
+      const recordKey = model.getKey() + ":" + counter;
+      let multi = this.client.multi();
+
+      for (let attributeName in flatObj) {
+        multi.hset(recordKey, attributeName, flatObj[attributeName]);
+      }
+      return multi.execAsync();
+    });
+  }
+
+  getAll(type) {
+    return this.client.getAsync(type);
+  }
+
+}
+
+module.exports = DbHelper;
--- server/routes/candidates.js
+++ server/routes/candidates.js
@@ -1,12 +1,46 @@
 import express from 'express';
-import db from '../helper/database';
+import DbHelper from '../helper/database';
+import faker from 'faker';
 
-var router = express.Router();
+const model = {
+  name: "candidates",
+  version: "1",
+  schema: {
+    firstName: "",
+    lastName: "",
+    shortDescription: "",
+    avatar: "",
+    comments: [],
+    lastContact: ""
+  },
+  getKey() {
+    return this.name + "_" + this.version
+  }
+};
 
-router.get('/', function(req, res, next) {
-  res.send('hello');   
+let candidate = express.Router();
+let db = new DbHelper();
+
+candidate.get('/', function(req, res, next) {
+  db.getAll(model);
 
   next();
 });
 
-module.exports = router;
+candidate.post('/populate', function (req, res, next) {
+  let fake_user = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    shortDescription: faker.lorem.sentence(),
+    avatar: faker.internet.avatar(),
+    comments: [],
+    lastContact: faker.date.past()
+  };
+
+  db.add(model, fake_user).then((result) => {
+    res.send(fake_user);
+  });
+
+});
+
+module.exports = candidate;
