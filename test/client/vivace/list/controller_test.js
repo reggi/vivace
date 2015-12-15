0ca0182a4864adfc1f7b82d228af119de0a36dbf
jimitpatel9 <jdp59@njit.edu>
2015-12-15 11:23:16 -0500

    ui-list updated and tested

--- test/client/vivace/list/controller_test.js
+++ test/client/vivace/list/controller_test.js
@@ -8,11 +8,9 @@ describe('the vivace list controller', () => {
     ctrl = $controller(require('../../../../client/vivace/list/controller'));
   }));
 
-  it('should not be null', () => {
-    chai.expect(ctrl).not.to.be.null;
+  it('should not be null users', () => {
+    chai.expect(ctrl.users).not.to.be.null;
   });
 
-  it('should have a default status of red', () => {
-    ctrl.status.should.equal('red');
-  });
+
 });
