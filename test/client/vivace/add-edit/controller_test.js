ea7aa5b94fde08bb3c8cef2a6e88a82ef296e101
jimitpatel9 <jdp59@njit.edu>
2015-12-15 17:30:27 -0500

    updated the add/edit page

--- test/client/vivace/add-edit/controller_test.js
+++ test/client/vivace/add-edit/controller_test.js
@@ -13,11 +13,11 @@ describe('the vivace list controller', () => {
       routeParams;
   beforeEach(angular.mock.module('irCandidateAddEdit'));
   beforeEach(angular.mock.inject(($controller, $rootScope, $routeParams) => {
-        ctrl = $controller(require('../../../../client/vivace/add-edit/controller'));
-        scope = $rootScope.$new();
-        routeParams = $routeParams;
-      }
-  ));
+      ctrl = $controller(require('../../../../client/vivace/add-edit/controller'));
+      scope = $rootScope.$new();
+      routeParams = $routeParams;
+    }
+  ))
 
   it('should not be update candidate for heading', () => {
     routeParams.id = 123;
