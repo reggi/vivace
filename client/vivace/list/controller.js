f72b60e83bc743cf5502436d38baf1bbf16d5f09
jimitpatel9 <jdp59@njit.edu>
2015-12-16 13:33:46 -0500

    removed conflicts

--- client/vivace/list/controller.js
+++ client/vivace/list/controller.js
@@ -1,38 +1,3 @@
 module.exports = function(dataStoreService) {
-
-  this.users =[
-    {
-      avatar : 'http://www.riomarca.com/clientes/captura/images/avatar.jpg',
-      firstName:'Jimit',
-      lastName:'Patel',
-      shortDescription:'Good Candidate',
-      email:'jpatel@inrhythm.com',
-      phone:'1234567890',
-      lastContact :'Last contacted a few seconds ago'
-    },
-    {
-      avatar : 'http://www.riomarca.com/clientes/captura/images/avatar.jpg',
-      firstName:'Jimit',
-      lastName:'Patel',
-      shortDescription:'Good Candidate',
-      email:'jpatel@inrhythm.com',
-      phone:'1234567890',
-      lastContact :'Last contacted a few seconds ago'
-
-    },
-    {
-      avatar : 'http://www.riomarca.com/clientes/captura/images/avatar.jpg',
-      firstName:'Jimit',
-      lastName:'Patel',
-      shortDescription:'Good Candidate',
-      email:'jpatel@inrhythm.com',
-      phone:'1234567890',
-      lastContact :'Last contacted a few seconds ago'
-
-    }
-  ];
-
-
-
-
+  this.users = dataStoreService.all('candidates');
 };
