ad8ea47d83301e0511c5a8d43358dd54b50be0f5
jimitpatel9 <jdp59@njit.edu>
2015-12-22 10:46:26 -0500

    bug solved email is now not required

--- client/vivace/list/controller.js
+++ client/vivace/list/controller.js
@@ -2,6 +2,7 @@ module.exports = function(CandidateModel, $location) {
   let promise = CandidateModel.query().$promise;
 
   promise.then((data) => {
+
     this.users = data;
   }, () => {
     $location.path('/not-found');
