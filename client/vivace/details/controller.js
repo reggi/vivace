0ebb2071780fe1250f168b15f96fe401d6ae7531
jimitpatel9 <jdp59@njit.edu>
2015-12-22 11:09:42 -0500

    removed comments and console.log

--- client/vivace/details/controller.js
+++ client/vivace/details/controller.js
@@ -1,11 +1,11 @@
 module.exports = function($location, CandidateModel, $routeParams) {
 
-  this.user = CandidateModel.get({id: $routeParams.id});
-  console.log(this.user);
-  /*promise.then((data) => {
+  let promise = CandidateModel.get({id: $routeParams.id}).$promise;
+
+  promise.then((data) => {
     this.user = data;
     this.user.name = data.firstName + ' ' + data.lastName;
   }, () => {
     $location.path('/not-found');
-  });*/
+  });
 };
