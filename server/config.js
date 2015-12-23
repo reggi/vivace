6631c76a1a02bfbe78d6fd55c0aacde328b3b43e
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-12-23 16:12:44 -0500

    -fixing config

--- server/config.js
+++ server/config.js
@@ -1,9 +1,8 @@
-var host = process.env.HOST || "localhost"
-var port = process.env.PORT || "8001"
+var domain_name = process.env.DOMAIN_NAME || "http://localhost:8001"
 module.exports = {
   redis_port : process.env.REDIS_PORT,
   redis_host : process.env.REDIS_HOST,
   oauth2_client_id : process.env.OAUTH2_CLIENT_ID,
   oauth2_client_secret : process.env.OAUTH2_CLIENT_SECRET,
-  oauth2_callback_url : "http://" + host + ":" + port + "/auth/google/callback"
+  oauth2_callback_url : domain_name + "/auth/google/callback"
 }
