7ed224cf554ea9a04c9b5ecd04155acd73d129e9
Jack Morgan <jackofseattle@gmail.com>
2015-12-29 14:27:15 -0500

    Release 1.0.1 server fix

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
@@ -7,7 +8,7 @@ import compression from 'compression';
 import {OAuth2Strategy as GoogleStrategy} from 'passport-google-oauth';
 import config from './config';
 
-import apiRouter from './routes';
+const app = express();
 
 app.use(cookieParser());
 
@@ -15,7 +16,6 @@ app.use(cookieParser());
 
 
 if (process.env.NODE_ENV !== 'production') {
-  let webpackConfig = require('../webpack.config');
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpack = require('webpack');
 
@@ -72,20 +72,20 @@ app.use('/api', require('./routes'));
 app.get('/',
   ensureAuthenticated,
   (req, res) => {
-    fs.readFile(path.join(__dirname, '../client/index.html'), (err, info) => {
-      res.type('html');
-      res.end(info);
-    });
-  }
+  fs.readFile(path.join(__dirname, '../client/index.html'), (err, info) => {
+  res.type('html');
+res.end(info);
+});
+}
 );
 
 app.get('/auth/google',
-    passport.authenticate('google', {
-      scope: [
+  passport.authenticate('google', {
+    scope: [
       'https://www.googleapis.com/auth/plus.login',
       'https://www.googleapis.com/auth/userinfo.email'
-      ]
-    })
+    ]
+  })
 );
 
 app.get('/auth/google/callback',
@@ -97,7 +97,7 @@ app.get('/auth/google/callback',
 );
 
 let listener = app.listen(process.env.PORT || 8001, () => {
-  console.log('\x1b[33m%s:\x1b[4m%s\x1b[0m', 'App is listening on port', listener.address().port);
+    console.log('\x1b[33m%s:\x1b[4m%s\x1b[0m', 'App is listening on port', listener.address().port);
 });
 
 
